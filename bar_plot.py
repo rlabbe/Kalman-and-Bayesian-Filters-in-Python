2f74f90c184521991b88270220f8574972e68147
Roger Labbe <rlabbejr@gmail.com>
2014-05-09 13:55:53 -0700

    Added ability to label the axes.

--- bar_plot.py
+++ bar_plot.py
@@ -7,10 +7,18 @@ Created on Fri May  2 12:21:40 2014
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(pos):
+def plot(pos, ylim=(0,1)):
     plt.cla()
     ax = plt.gca()
     x = np.arange(len(pos))
     ax.bar(x, pos)
-    plt.ylim([0,1])
-    plt.xticks(x+0.5, x)
+    if ylim:
+        plt.ylim([0,1])
+    plt.xticks(x+0.4, x)
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    p = [0.2245871, 0.06288015, 0.06109133, 0.0581008, 0.09334062, 0.2245871,
+     0.06288015, 0.06109133, 0.0581008,  0.09334062]*2
+    plot(p)
