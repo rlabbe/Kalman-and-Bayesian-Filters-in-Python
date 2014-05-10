09fa08f73e337c0d4a78b5978fc1cd44bdc62797
Roger Labbe <rlabbejr@gmail.com>
2014-05-10 11:25:52 -0700

    mv'ed gaussian.py to stats.py
    The name was horrible, we were using gaussian.gaussian() to compute
    gaussians. stats.gaussian() is much better. Updated all notebooks
    and scripts to reflect the change.

--- gaussian_internal.py
+++ gaussian_internal.py
@@ -9,7 +9,7 @@ import math
 import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
 import numpy as np
-import gaussian as g
+import stats
 
 def plot_gaussian (mu, variance,
                    mu_line=False,
@@ -17,7 +17,7 @@ def plot_gaussian (mu, variance,
                    xlabel=None,
                    ylabel=None):
     xs = np.arange(mu-variance*2,mu+variance*2,0.1)
-    ys = [g.gaussian (x, mu, variance)*100 for x in xs]
+    ys = [stats.gaussian (x, mu, variance)*100 for x in xs]
     plt.plot (xs, ys)
     if mu_line:
         plt.axvline(mu)
@@ -34,12 +34,12 @@ def display_stddev_plot():
     pylab.rcParams['figure.figsize'] = 12,6
     xs = np.arange(10,30,0.1)
     var = 8; stddev = math.sqrt(var)
-    p2, = plt.plot (xs,[g.gaussian(x, 20, var) for x in xs])
+    p2, = plt.plot (xs,[stats.gaussian(x, 20, var) for x in xs])
     x = 20+stddev
-    y = g.gaussian(x, 20, var)
+    y = stats.gaussian(x, 20, var)
     plt.plot ([x,x], [0,y],'g')
     plt.plot ([20-stddev, 20-stddev], [0,y], 'g')
-    y = g.gaussian(20,20,var)
+    y = stats.gaussian(20,20,var)
     plt.plot ([20,20],[0,y],'b')
     ax = plt.axes()
     ax.annotate('68%', xy=(20.3, 0.045))
