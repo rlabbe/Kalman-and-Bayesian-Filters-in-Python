3fa8a95ae03edaf474d995a6e0dfa7d1b9939443
Roger Labbe <rlabbejr@gmail.com>
2014-05-28 23:15:39 -0700

    Lots of minor layout edits. Generated new PDF of book.

--- build_book.bat
+++ build_book.bat
@@ -1,5 +1,6 @@
 
-python merge_book.py Preface.ipynb Signals_and_Noise.ipynb g-h_filter.ipynb discrete_bayes.ipynb Gaussians.ipynb Kalman_Filters.ipynb Multidimensional_Kalman_Filters.ipynb Kalman_Filter_Math.ipynb Designing_Kalman_Filters.ipynb Extended_Kalman_Filters.ipynb Unscented_Kalman_Filter.ipynb > book.ipynb
+python merge_book.py Preface.ipynb Signals_and_Noise.ipynb g-h_filter.ipynb discrete_bayes.ipynb Gaussians.ipynb Kalman_Filters.ipynb Multidimensional_Kalman_Filters.ipynb Kalman_Filter_Math.ipynb Designing_Kalman_Filters.ipynb Extended_Kalman_Filters.ipynb Unscented_Kalman_Filter.ipynb >Kalman_and_Bayesian_Filters_in_Python.ipynb 
+
 ipython nbconvert --to latex --template book --post PDF Kalman_and_Bayesian_Filters_in_Python.ipynb
 
 
