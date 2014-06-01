84c2534d506b8905127cb2d4ec27aa4467ae0b06
Roger Labbe <rlabbejr@gmail.com>
2014-05-31 17:57:33 -0700

    Changed variables in code to be more readable; added comments.

--- UKF.py
+++ UKF.py
@@ -9,7 +9,7 @@ from numpy import matrix, zeros, asmatrix, size
 from  numpy.linalg import cholesky
 
 
-def sigma_points (mu, cov, kappa):
+def sigma_points (mean, covariance, kappa):
     """ Computes the sigma points and weights for an unscented Kalman filter.
     xm are the means, and P is the covariance. kappa is an arbitrary constant
     constant. Returns tuple of the sigma points and weights.
@@ -22,8 +22,8 @@ def sigma_points (mu, cov, kappa):
     sigma_points ([5, 2], 9*eye(2), 2) # means 5 and 2, covariance 9I
     """
 
-    mu  = asmatrix(mu)
-    cov = asmatrix(cov)
+    mean = asmatrix(mean)
+    covariance = asmatrix(covariance)
 
     n = size(mu)
 
@@ -39,46 +39,61 @@ def sigma_points (mu, cov, kappa):
 
     # use cholesky to find matrix square root of (n+kappa)*cov
     #  U'*U = (n+kappa)*P
-    U = asmatrix (cholesky((n+kappa)*cov))
+    U = asmatrix (cholesky((n+kappa)*covariance))
 
     # mean is in location 0.
-    Xi[:,0] = mu
+    Xi[:,0] = mean
+
+    for col in range (n):
+        Xi[:, col+1] = mean + U[:, col]
 
     for k in range (n):
-        Xi[:,k+1] = mu + U[:,k]
-
-    for k in range (n):
-        Xi[:, n+k+1] = mu - U[:,k]
+        Xi[:, n+col+1] = mean - U[:, col]
 
     return (Xi, W)
 
 
 
 def unscented_transform (Xi, W, NoiseCov=None):
-    """ computes the unscented transform of a set of signma points and weights.
-    returns the mean and covariance in a tuple
+    """ computes the unscented transform of a set of sigma points 'X'
+    and weights 'W'.
+
+    W should be in the form:
+
+       [w0, w1, w2,...wn].T
+
+    where w0 is the mean,
+
+    Xi should be in the form:
+       [X_00,
+
+
+    returns the mean and covariance in a tuple '(mean, cov)'
     """
-    W = asmatrix(W)
+
+    W  = asmatrix(W)
     Xi = asmatrix(Xi)
 
     n, kmax = Xi.shape
 
     # initialize results to 0
-    mu  = matrix (zeros((n,1)))
-    cov = matrix (zeros((n,n)))
+    mean = matrix (zeros((n,1)))
+    cov  = matrix (zeros((n,n)))
 
     for k in range (kmax):
-        mu += W[0,k] * Xi[:,k]
+        mean += W[0,k] * Xi[:,k]
 
     for k in range (kmax):
         cov += W[0,k]*(Xi[:,k]-mu) * (Xi[:,k]-mu).T
 
-    return (mu, cov)
+    return (mean, cov)
 
 
-xi = matrix ([[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7]])
-mu = matrix ([1,2,3,4,5,6,7])
+if __name__ == "__main__":
 
-m,c = unscented_transform(xi, mu)
-print m
-print c
+    xi = matrix ([[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7]])
+    mu = matrix ([1,2,3,4,5,6,7])
+
+    m,c = unscented_transform(xi, mu)
+    print m
+    print c
