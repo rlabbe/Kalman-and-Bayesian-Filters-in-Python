d8df25fe7b6f903db044a29d42eb6f4ae4c81820
Roger Labbe <rlabbejr@gmail.com>
2014-08-22 10:11:06 -0700

    Symbology changes.
    Working on finalizing the symbology used in this book. Added appendix
    to document what other books in the field use for the Kalman filter
    equations.

--- mkf_internal.py
+++ mkf_internal.py
@@ -11,20 +11,20 @@ import stats
 
 def show_residual_chart():
     plt.xlim([0.9,2.5])
-    plt.ylim([0.5,2.5])
+    plt.ylim([1.5,3.5])
 
-    plt.scatter ([1,2,2],[1,2,1.3])
-    plt.scatter ([2],[1.8],marker='o')
+    plt.scatter ([1,2,2],[2,3,2.3])
+    plt.scatter ([2],[2.8],marker='o')
     ax = plt.axes()
-    ax.annotate('', xy=(2,2), xytext=(1,1),
+    ax.annotate('', xy=(2,3), xytext=(1,2),
                 arrowprops=dict(arrowstyle='->', ec='b',shrinkA=3, shrinkB=4))
-    ax.annotate('prediction', xy=(2.04,2.), color='b')
-    ax.annotate('measurement', xy=(2.05, 1.28))
-    ax.annotate('prior measurement', xy=(1, 0.9))
-    ax.annotate('residual', xy=(2.04,1.6), color='r')
-    ax.annotate('new estimate', xy=(2,1.8),xytext=(2.1,1.8),
+    ax.annotate('prediction', xy=(2.04,3.), color='b')
+    ax.annotate('measurement', xy=(2.05, 2.28))
+    ax.annotate('prior estimate', xy=(1, 1.9))
+    ax.annotate('residual', xy=(2.04,2.6), color='r')
+    ax.annotate('new estimate', xy=(2,2.8),xytext=(2.1,2.8),
                 arrowprops=dict(arrowstyle='->', ec="k", shrinkA=3, shrinkB=4))
-    ax.annotate('', xy=(2,2), xytext=(2,1.3),
+    ax.annotate('', xy=(2,3), xytext=(2,2.3),
                 arrowprops=dict(arrowstyle="-",
                                 ec="r",
                                 shrinkA=5, shrinkB=5))
@@ -124,4 +124,4 @@ def show_x_with_unobserved():
 
 if __name__ == "__main__":
 
-    show_x_with_unobserved()
+    show_residual_chart()
