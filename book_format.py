9f3c2c0fa739aa51a8d0039b6795d53c25952ab1
Roger Labbe <rlabbejr@gmail.com>
2014-08-31 22:07:01 -0700

    Altered colors of plots.
    Maybe not perfect yet, but I have signicantly changed the colors
    and various other look-and-fell aspects of the plots.

--- book_format.py
+++ book_format.py
@@ -6,18 +6,16 @@ def load_style():
     styles = open("./styles/custom2.css", "r").read()
     return HTML(styles)
 
-pylab.rcParams['axes.color_cycle'] = '348ABD, 7A68A6, A60628, 467821, CF4457, 188487, E24A33'
-pylab.rcParams['lines.linewidth'] = 1
-
+pylab.rcParams['lines.linewidth'] = 2
 pylab.rcParams['lines.antialiased'] = True
 pylab.rcParams['patch.linewidth'] = 0.5
 pylab.rcParams['patch.facecolor'] = '348ABD' #blue
 pylab.rcParams['patch.edgecolor'] = 'eeeeee'
 pylab.rcParams['patch.antialiased'] = True
 pylab.rcParams['font.family'] = 'monospace'
-pylab.rcParams['font.size'] = 10.0
+pylab.rcParams['font.size'] = 12.0
 #pylab.rcParams['font.monospace'] = 'Andale Mono, Nimbus Mono L, Courier New, Courier, Fixed, Terminal, monospace'
-pylab.rcParams['axes.facecolor'] = 'eeeeee'
+pylab.rcParams['axes.facecolor'] = 'E5E5E5'
 pylab.rcParams['axes.edgecolor'] = 'bcbcbc'
 pylab.rcParams['axes.linewidth'] = 1
 pylab.rcParams['axes.grid'] = True
@@ -25,7 +23,7 @@ pylab.rcParams['axes.titlesize'] = 'x-large'
 pylab.rcParams['axes.labelsize'] = 'large'
 pylab.rcParams['axes.labelcolor'] = '555555'
 pylab.rcParams['axes.axisbelow'] = True
-pylab.rcParams['axes.color_cycle'] = '348ABD, 7A68A6, A60628, 467821, CF4457, 188487, E24A33'
+pylab.rcParams['axes.color_cycle'] = '004080, 8EBA42, E24A33, 348ABD, 777760, 988ED5, FDC15E'
 pylab.rcParams['xtick.major.pad'] = 6
 pylab.rcParams['xtick.minor.size'] = 0
 pylab.rcParams['xtick.minor.pad'] = 6
@@ -36,4 +34,8 @@ pylab.rcParams['figure.facecolor'] = '0.85'
 pylab.rcParams['figure.edgecolor'] = '0.50'
 pylab.rcParams['figure.subplot.hspace'] = 0.5
 pylab.rcParams['figure.figsize'] = 12,6
+pylab.rcParams['grid.color'] = 'ffffff'
+pylab.rcParams['grid.linestyle'] = 'solid'
+pylab.rcParams['grid.linewidth'] = 1.5
+
 
