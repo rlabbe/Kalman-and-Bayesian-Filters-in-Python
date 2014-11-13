d98c85a24b97d1a200a0d636c924b98af7054c61
Roger Labbe <rlabbejr@gmail.com>
2014-11-13 07:20:43 -0800

    Made sensor fusion code a function.
    The function takes the wheel and gps variance in as parameters
    so I can easily vary the values and display them to the reader.

--- merge_book.py
+++ merge_book.py
@@ -43,17 +43,17 @@ if __name__ == '__main__':
     #merge_notebooks(sys.argv[1:])
     merge_notebooks(
         ['Preface.ipynb',
-         'Chapter01_gh_filter/g-h_filter.ipynb',
-         'Chapter02_Discrete_Bayes/discrete_bayes.ipynb',
-         'Chapter03_Least_Squares/Least_Squares_Filters.ipynb',
-         'Chapter04_Gaussians/Gaussians.ipynb',
-         'Chapter05_Kalman_Filters/Kalman_Filters.ipynb',
-         'Chapter06_Multivariate_Kalman_Filter/Multivariate_Kalman_Filters.ipynb',
-         'Chapter07_Kalman_Filter_Math/Kalman_Filter_Math.ipynb',
-         'Chapter08_Designing_Kalman_Filters/Designing_Kalman_Filters.ipynb',
-         'Chapter09_Extended_Kalman_Filters/Extended_Kalman_Filters.ipynb',
-         'Chapter10_Unscented_Kalman_Filters/Unscented_Kalman_Filter.ipynb',
-         'Chapter11_Designing_Nonlinear_Kalman_Filters/Designing_Nonlinear_Kalman_Filters.ipynb',
+         '01_gh_filter/g-h_filter.ipynb',
+         '02_Discrete_Bayes/discrete_bayes.ipynb',
+         '03_Least_Squares/Least_Squares_Filters.ipynb',
+         '04_Gaussians/Gaussians.ipynb',
+         '05_Kalman_Filters/Kalman_Filters.ipynb',
+         '06_Multivariate_Kalman_filter/Multivariate_Kalman_Filters.ipynb',
+         '07_Kalman_Filter_Math/Kalman_Filter_Math.ipynb',
+         '08_Designing_Kalman_Filters/Designing_Kalman_Filters.ipynb',
+         '09_Extended_Kalman_Filters/Extended_Kalman_Filters.ipynb',
+         '10_Unscented_Kalman_Filters/Unscented_Kalman_Filter.ipynb',
+         '11_Designing_Nonlinear_Kalman_Filters/Designing_Nonlinear_Kalman_Filters.ipynb',
          'Appendix_A_Installation/Appendix_Installation.ipynb',
          'Appendix_B_Symbols_and_Notations/Appendix_Symbols_and_Notations.ipynb'])
 
