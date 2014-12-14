b8cf2632e9bd33a33a528426259fc142db5f80de
Roger Labbe <rlabbejr@gmail.com>
2014-12-13 20:05:06 -0800

    Batch processing and RTS.
    This belonged with the last check in. Added desciptions on
    batch processing and using the RTS smoothing algorithm.

--- code/book_format.py
+++ code/book_format.py
@@ -3,9 +3,33 @@ from IPython.core.display import HTML
 import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
 
+
+def test_filterpy_version():
+    import filterpy
+    min_version = [0,0,10]
+    v = filterpy.__version__
+    tokens = v.split('.')
+    for i,v in enumerate(tokens):
+        if int(v) > min_version[i]:
+            return
+
+    i = len(tokens) - 1
+    if min_version[i] > int(tokens[i]):
+       raise Exception("Minimum FilterPy version supported is {}.{}.{}.\n"
+                       "Please install a more recent version." .format(
+             *min_version))
+    v = int(tokens[0]*1000)
+
 def load_style(name='../styles/custom2.css'):
     styles = open(name, 'r').read()
     return HTML(styles)
+
+
+# ensure that we have the correct filterpy loaded. This is
+# called when this module is imported at the top of each book
+# chapter so the reader can see that they need to update FilterPy.
+test_filterpy_version()
+
 
 pylab.rcParams['lines.linewidth'] = 2
 pylab.rcParams['lines.antialiased'] = True
