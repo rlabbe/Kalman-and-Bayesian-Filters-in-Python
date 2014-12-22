6a3595a1901e6429f08f85be6e682b032109624d
Roger Labbe <rlabbejr@gmail.com>
2014-12-22 12:15:19 -0800

    Switched to ggplot style plotting.

--- code/book_format.py
+++ code/book_format.py
@@ -30,39 +30,9 @@ def load_style(name='../styles/custom2.css'):
 # chapter so the reader can see that they need to update FilterPy.
 test_filterpy_version()
 
+'''
 
-pylab.rcParams['lines.linewidth'] = 2
-pylab.rcParams['lines.antialiased'] = True
-pylab.rcParams['patch.linewidth'] = 0.5
-pylab.rcParams['patch.facecolor'] = '348ABD' #blue
-pylab.rcParams['patch.edgecolor'] = 'eeeeee'
-pylab.rcParams['patch.antialiased'] = True
-pylab.rcParams['font.family'] = 'monospace'
-pylab.rcParams['font.size'] = 12.0
-#pylab.rcParams['font.monospace'] = 'Andale Mono, Nimbus Mono L, Courier New, Courier, Fixed, Terminal, monospace'
-pylab.rcParams['axes.facecolor'] = 'E5E5E5'
-pylab.rcParams['axes.edgecolor'] = 'bcbcbc'
-pylab.rcParams['axes.linewidth'] = 1
-pylab.rcParams['axes.grid'] = True
-pylab.rcParams['axes.titlesize'] = 'x-large'
-pylab.rcParams['axes.labelsize'] = 'large'
-pylab.rcParams['axes.labelcolor'] = '555555'
-pylab.rcParams['axes.axisbelow'] = True
-pylab.rcParams['axes.color_cycle'] = '004080, 8EBA42, E24A33, 348ABD, 777760, 988ED5, FDC15E'
-pylab.rcParams['xtick.major.pad'] = 6
-pylab.rcParams['xtick.minor.size'] = 0
-pylab.rcParams['xtick.minor.pad'] = 6
-pylab.rcParams['xtick.color'] = '555555'
-pylab.rcParams['ytick.direction'] = 'in'
-pylab.rcParams['legend.fancybox'] = True
-pylab.rcParams['figure.facecolor'] = '1.0'
-pylab.rcParams['figure.edgecolor'] = '0.50'
-pylab.rcParams['figure.subplot.hspace'] = 0.5
-pylab.rcParams['figure.figsize'] = 12,6
-pylab.rcParams['grid.color'] = 'ffffff'
-pylab.rcParams['grid.linestyle'] = 'solid'
-pylab.rcParams['grid.linewidth'] = 1.5
-
+'''
 
 def equal_axis():
     pylab.rcParams['figure.figsize'] = 10,10
@@ -70,4 +40,44 @@ def equal_axis():
 
 def reset_axis():
     pylab.rcParams['figure.figsize'] = 12, 6
+
+try:
+    plt.style.use('ggplot')
+    reset_axis ()
+    pylab.rcParams['lines.linewidth'] = 2
+    pylab.rcParams['figure.subplot.hspace'] = 0.5
+
+except:
+    pylab.rcParams['lines.linewidth'] = 2
+    pylab.rcParams['lines.antialiased'] = True
+    pylab.rcParams['patch.linewidth'] = 0.5
+    pylab.rcParams['patch.facecolor'] = '348ABD' #blue
+    pylab.rcParams['patch.edgecolor'] = 'eeeeee'
+    pylab.rcParams['patch.antialiased'] = True
+    pylab.rcParams['font.family'] = 'monospace'
+    pylab.rcParams['font.size'] = 12.0
+    #pylab.rcParams['font.monospace'] = 'Andale Mono, Nimbus Mono L, Courier New, Courier, Fixed, Terminal, monospace'
+    pylab.rcParams['axes.facecolor'] = 'E5E5E5'
+    pylab.rcParams['axes.edgecolor'] = 'bcbcbc'
+    pylab.rcParams['axes.linewidth'] = 1
+    pylab.rcParams['axes.grid'] = True
+    pylab.rcParams['axes.titlesize'] = 'x-large'
+    pylab.rcParams['axes.labelsize'] = 'large'
+    pylab.rcParams['axes.labelcolor'] = '555555'
+    pylab.rcParams['axes.axisbelow'] = True
+    pylab.rcParams['axes.color_cycle'] = '004080, 8EBA42, E24A33, 348ABD, 777760, 988ED5, FDC15E'
+    pylab.rcParams['xtick.major.pad'] = 6
+    pylab.rcParams['xtick.minor.size'] = 0
+    pylab.rcParams['xtick.minor.pad'] = 6
+    pylab.rcParams['xtick.color'] = '555555'
+    pylab.rcParams['ytick.direction'] = 'in'
+    pylab.rcParams['legend.fancybox'] = True
+    pylab.rcParams['figure.facecolor'] = '1.0'
+    pylab.rcParams['figure.edgecolor'] = '0.50'
+    pylab.rcParams['figure.subplot.hspace'] = 0.5
+    pylab.rcParams['figure.figsize'] = 12,6
+    pylab.rcParams['grid.color'] = 'ffffff'
+    pylab.rcParams['grid.linestyle'] = 'solid'
+    pylab.rcParams['grid.linewidth'] = 1.5
+
 
