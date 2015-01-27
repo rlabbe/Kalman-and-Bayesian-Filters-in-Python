9db6b3f69ddb71dbc9cbeba8c570b7159fc2bcd1
Roger Labbe <rlabbejr@gmail.com>
2015-01-27 15:46:00 -0800

    fixed to work with reorganized files

--- pdf/update_pdf.sh
+++ pdf/update_pdf.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
+cd ..
 git checkout gh-pages
 git checkout master Kalman_and_Bayesian_Filters_in_Python.pdf
 git add Kalman_and_Bayesian_Filters_in_Python.pdf
 git commit -m 'updating PDF'
 git push
 git checkout master
+cd pdf
