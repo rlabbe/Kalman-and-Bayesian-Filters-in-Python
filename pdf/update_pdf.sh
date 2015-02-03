170804ba7b47832e7d3e36ca7169eb8e50e8c8c7
Roger Labbe <rlabbejr@gmail.com>
2015-02-02 19:36:00 -0800

    Trying to update README.md on github.io pages.

--- pdf/update_pdf.sh
+++ pdf/update_pdf.sh
@@ -2,7 +2,9 @@
 cd ..
 git checkout gh-pages
 git checkout master Kalman_and_Bayesian_Filters_in_Python.pdf
+git checkout README.md
 git add Kalman_and_Bayesian_Filters_in_Python.pdf
+git add README.md
 git commit -m 'updating PDF'
 git push
 git checkout master
