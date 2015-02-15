584330659810c2cf88a1486b99083a0df9a9f8d8
Roger Labbe <rlabbejr@gmail.com>
2015-02-15 09:04:30 -0800

    added a pull before the push

--- pdf/update_pdf.sh
+++ pdf/update_pdf.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ..
 git checkout gh-pages
+git pull
 git checkout master Kalman_and_Bayesian_Filters_in_Python.pdf
 git checkout master README.md
 git add Kalman_and_Bayesian_Filters_in_Python.pdf
