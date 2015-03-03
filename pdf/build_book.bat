e1c3e8a8eb2f725e00d7d1b105669d06749f86d7
Roger Labbe <rlabbejr@gmail.com>
2015-03-03 13:18:37 -0800

    Added full ipynb and html for performance check.
    The mathjax takes forever to render in the notebook. As in
    it's been going for 38 minutes and it is still not done.

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -2,6 +2,6 @@
 python merge_book.py >Kalman_and_Bayesian_Filters_in_Python.ipynb
 
 
-ipython nbconvert --to latex --template book --post PDF Kalman_and_Bayesian_Filters_in_Python.ipynb
+ipython nbconvert --to pdf --template book Kalman_and_Bayesian_Filters_in_Python.ipynb
 
 
