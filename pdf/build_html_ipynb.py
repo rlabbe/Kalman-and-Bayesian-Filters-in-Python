0343c45445a38c15a74aef2e844154c58690e821
Roger Labbe <rlabbejr@gmail.com>
2015-03-07 22:41:39 -0800

    Playing with html generation
    I think I prefer just letting nbconvert generating static files,
    but if I want to switch to using Bokeh or other javascript based
    rendering I'll need the HTML conversion facility.

--- pdf/build_html_ipynb.py
+++ pdf/build_html_ipynb.py
@@ -1,38 +1,33 @@
+
 from __future__ import print_function
-import io
 import IPython.nbformat as nbformat
-import sys
+
 from formatting import *
 
 
-def merge_notebooks(filenames):
-    merged = None
+def prep_for_html_conversion(filename):
     added_appendix = False
-    for fname in filenames:
-        with io.open(fname, 'r', encoding='utf-8') as f:
-            nb = nbformat.read(f, nbformat.NO_CONVERT)
-            remove_formatting(nb)
-            if not added_appendix and fname[0:8] == 'Appendix':
-                remove_links_add_appendix(nb)
-                added_appendix = True
-            else:
-                remove_links(nb)
-        if merged is None:
-            merged = nb
+    with io.open('../'+filename, 'r', encoding='utf-8') as f:
+        nb = nbformat.read(f, nbformat.NO_CONVERT)
+        remove_formatting(nb)
+        if not added_appendix and filename[0:8] == 'Appendix':
+            remove_links_add_appendix(nb)
+            added_appendix = True
         else:
-            merged.cells.extend(nb.cells)
-    #merged.metadata.name += "_merged"
-
-    print(nbformat.writes(merged, nbformat.NO_CONVERT))
-
+            remove_links(nb)
+        nbformat.write(nb, filename)
+ 
 
 if __name__ == '__main__':
-    '''merge_notebooks(
-        ['../00_Preface.ipynb',
-         '../01_g-h_filter.ipynb',
-         '../Appendix_A_Installation.ipynb'])'''
+    notebooks = \
+        ['00_Preface.ipynb',
+         '01_g-h_filter.ipynb',
+         'Appendix_A_Installation.ipynb']
+         
+    for n in notebooks:
+        prep_for_html_conversion(n)
 
-    merge_notebooks(
+    '''merge_notebooks(
         ['../00_Preface.ipynb',
          '../01_g-h_filter.ipynb',
          '../02_Discrete_Bayes.ipynb',
@@ -51,4 +46,4 @@ if __name__ == '__main__':
          '../15_HInfinity_Filters.ipynb',
          '../16_Ensemble_Kalman_Filters.ipynb',
          '../Appendix_A_Installation.ipynb',
-         '../Appendix_B_Symbols_and_Notations.ipynb'])
+         '../Appendix_B_Symbols_and_Notations.ipynb'])'''
