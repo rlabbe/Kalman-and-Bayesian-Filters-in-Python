7f2b152c9dad5fa95584980252bd5d6bdd5dacde
Roger Labbe <rlabbejr@gmail.com>
2015-04-04 10:30:50 -0700

    Fixed PDF generation in Windows

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -1,7 +1,6 @@
-
-python merge_book.py >Kalman_and_Bayesian_Filters_in_Python.ipynb
-
-
-ipython nbconvert --to pdf --template book Kalman_and_Bayesian_Filters_in_Python.ipynb
+python merge_book.py
+ipython nbconvert --to latex --template book book.ipynb
+ipython to_pdf.py
+move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
 
 
