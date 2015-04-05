6bca6b56ec88914186266b2b0d28bdf8c48ad76e
Roger Labbe <rlabbejr@gmail.com>
2015-04-05 12:29:23 -0700

    Converted update_pdf to windows bat file

---
+++ pdf/update_pdf.bat
@@ -0,0 +1,12 @@
+#! /bin/bash
+cd ..
+git checkout gh-pages
+git pull
+git checkout master Kalman_and_Bayesian_Filters_in_Python.pdf
+git checkout master README.md
+git add Kalman_and_Bayesian_Filters_in_Python.pdf
+git add README.md
+git commit -m 'updating PDF'
+git push
+git checkout master
+cd pdf
