9571f23094e23fdb865bf802926ce317f1d5b4ab
Roger Labbe <rlabbejr@gmail.com>
2015-05-01 10:49:12 -0700

    Code cleanup
    Removed commented out code and unneeded imports.

--- pdf/to_pdf.py
+++ pdf/to_pdf.py
@@ -1,15 +1,4 @@
-from __future__ import print_function
-import io
-
 import IPython.nbconvert.exporters.pdf as pdf
-import fileinput
-
-'''
-for line in fileinput.input('book.tex', openhook=fileinput.hook_encoded("iso-8859-1")):
-    #print(line.replace('\chapter{Preface}\label{preface}', '\chapter*{Preface}\label{preface}'), end='')
-#    line.replace('    \chapter{Preface}\label{preface}', '    \chapter*{Preface}\label{preface}')
-    line.replace('shit', 'poop')
-'''
 
 f = open('book.tex', 'r',  encoding="iso-8859-1")
 filedata = f.read()
@@ -17,7 +6,7 @@ f.close()
 
 newdata = filedata.replace('\chapter{Preface}', '\chapter*{Preface}')
 
-f = open('book.tex', 'w',  encoding="iso-8859-1")
+f = open('book.tex', 'w', encoding="iso-8859-1")
 f.write(newdata)
 f.close()
 
