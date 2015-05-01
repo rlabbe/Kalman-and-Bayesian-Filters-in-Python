172552335721b8414c6e1caf07d29640a56f3e77
Roger Labbe <rlabbejr@gmail.com>
2015-05-01 10:42:53 -0700

    Fixed PDF output; blank Particle filter chapter.
    The PDF was not setting the Preface as a unnumbered chapter.
    I think there is more work to get it's subsections unnumbered.
    
    Added empty Particl filter chapter. Had to renumber the chapters
    to make it fit in.

--- pdf/to_pdf.py
+++ pdf/to_pdf.py
@@ -4,10 +4,22 @@ import io
 import IPython.nbconvert.exporters.pdf as pdf
 import fileinput
 
+'''
 for line in fileinput.input('book.tex', openhook=fileinput.hook_encoded("iso-8859-1")):
-#    print(line.replace('\chapter{Preface}', '\chapter*{Preface}'), end='')
-    line.replace('\chapter{Preface}', '\chapter*{Preface}')
+    #print(line.replace('\chapter{Preface}\label{preface}', '\chapter*{Preface}\label{preface}'), end='')
+#    line.replace('    \chapter{Preface}\label{preface}', '    \chapter*{Preface}\label{preface}')
+    line.replace('shit', 'poop')
+'''
 
+f = open('book.tex', 'r',  encoding="iso-8859-1")
+filedata = f.read()
+f.close()
+
+newdata = filedata.replace('\chapter{Preface}', '\chapter*{Preface}')
+
+f = open('book.tex', 'w',  encoding="iso-8859-1")
+f.write(newdata)
+f.close()
 
 p = pdf.PDFExporter()
 p.run_latex('book.tex')
