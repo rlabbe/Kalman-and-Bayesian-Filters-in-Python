335df7f42de798a85f0de9cc387d2f2536ed300c
Roger Labbe <rlabbejr@gmail.com>
2015-07-15 19:03:59 -0700

    Fixed bug in EKF residual function.
    Comment was wrong, it normalizes to [-pi, pi].
    
    Also, code didn't handle cases where a-b > 2Pi

--- code/book_format.py
+++ code/book_format.py
@@ -12,7 +12,7 @@ sys.path.insert(0,'./code') # allow us to import book_format
 
 def test_filterpy_version():
     import filterpy
-    min_version = [0,0,22]
+    min_version = [0,0,25]
     v = filterpy.__version__
     tokens = v.split('.')
     for i,v in enumerate(tokens):
