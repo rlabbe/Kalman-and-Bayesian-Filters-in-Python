b0a4b5d159e7552c63ff22e9f33a4200ec4a81e7
Roger Labbe <rlabbejr@gmail.com>
2015-08-08 17:22:04 -0700

    Updated to require FilterPy 0.0.26

--- code/book_format.py
+++ code/book_format.py
@@ -30,7 +30,7 @@ sys.path.insert(0, './code') # allow us to import book_format
 
 def test_filterpy_version():
     import filterpy
-    min_version = [0,0,25]
+    min_version = [0,0,26]
     v = filterpy.__version__
     tokens = v.split('.')
     for i,v in enumerate(tokens):
