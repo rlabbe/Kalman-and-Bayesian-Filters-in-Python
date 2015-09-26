3952a7501e7bbdff90c51b21f80f6a86fd319d6b
Roger Labbe <rlabbejr@gmail.com>
2015-09-26 08:11:52 -0700

    Added code to also build the 6x9 PDF.

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -1,6 +1,9 @@
 python merge_book.py
+
+ipython nbconvert --to latex --template book6x9 book.ipynb
+ipython to_pdf.py
+move /Y book.pdf ../book6x9.pdf
+
 ipython nbconvert --to latex --template book book.ipynb
 ipython to_pdf.py
 move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
-
-
