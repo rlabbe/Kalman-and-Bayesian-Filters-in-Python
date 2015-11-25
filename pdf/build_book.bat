d11798b0d80a9a8b6eb37c2658611eae2b04a3ab
Roger Labbe <rlabbejr@gmail.com>
2015-11-25 12:02:48 -0800

    Fixed name of the 6x9 book file.

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -2,7 +2,7 @@ python merge_book.py
 
 ipython nbconvert --to latex --template book6x9 book.ipynb
 ipython to_pdf.py
-move /Y book.pdf ../book6x9.pdf
+move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python6x9.pdf
 
 ipython nbconvert --to latex --template book book.ipynb
 ipython to_pdf.py
