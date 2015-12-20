0e86469faceee500f82d5bd4ed631e54020d60a4
Roger Labbe <rlabbejr@gmail.com>
2015-12-20 08:59:11 -0800

    PDF build scripts build 6x9 book separately.

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -1,9 +1,5 @@
 python merge_book.py
 
-ipython nbconvert --to latex --template book6x9 book.ipynb
-ipython to_pdf.py
-move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python6x9.pdf
-
 ipython nbconvert --to latex --template book book.ipynb
 ipython to_pdf.py
 move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
