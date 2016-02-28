590af94807979d602aba1a8e540acfcb51bba44d
Roger Labbe <rlabbejr@gmail.com>
2016-02-27 18:55:34 -0800

    Switching to interactive plots

--- code/book_plots.py
+++ code/book_plots.py
@@ -236,7 +236,6 @@ def plot_estimate_chart_3():
 def create_predict_update_chart(box_bg = '#CCCCCC',
                 arrow1 = '#88CCFF',
                 arrow2 = '#88FF88'):
-    plt.close('all')
     plt.figure(figsize=(4, 2.), facecolor='w')
     #plt.figure(figsize=(14,12.5), facecolor='w')
     ax = plt.axes((0, 0, 1, 1),
@@ -299,6 +298,50 @@ def create_predict_update_chart(box_bg = '#CCCCCC',
     plt.axis('equal')
     plt.xlim(2,10)
     plt.savefig('../figs/gh_predict_update.png', pad_inches=0.1)
+
+
+def show_residual_chart(show_eq=True, show_H=False):
+    plt.figure(figsize=(11, 3.), facecolor='w')
+    est_y = ((164.2-158)*.8 + 158)
+
+    ax = plt.axes(xticks=[], yticks=[], frameon=False)
+    ax.annotate('', xy=[1,159], xytext=[0,158],
+                arrowprops=dict(arrowstyle='->',
+                                ec='r', lw=3, shrinkA=6, shrinkB=5))
+
+    ax.annotate('', xy=[1,159], xytext=[1,164.2],
+                arrowprops=dict(arrowstyle='-',
+                                ec='k', lw=3, shrinkA=8, shrinkB=8))
+
+    ax.annotate('', xy=(1., est_y), xytext=(0.9, est_y),
+                arrowprops=dict(arrowstyle='->', ec='#004080',
+                                lw=2,
+                                shrinkA=3, shrinkB=4))
+
+
+    plt.scatter ([0,1], [158.0,est_y], c='k',s=128)
+    plt.scatter ([1], [164.2], c='b',s=128)
+    plt.scatter ([1], [159], c='r', s=128)
+    plt.text (1.05, 158.8, r"prior $(\bar{x}_t)$", ha='center',va='top',fontsize=18,color='red')
+    plt.text (0.5, 159.6, "prediction", ha='center',va='top',fontsize=18,color='red')
+    plt.text (1.0, 164.4, r"measurement ($z$)",ha='center',va='bottom',fontsize=18,color='blue')
+    plt.text (0, 157.8, r"posterior ($x_{t-1}$)", ha='center', va='top',fontsize=18)
+    plt.text (1.02, est_y-1.5, "residual($y$)", ha='left', va='center',fontsize=18)
+    if show_eq:
+        if show_H:
+            plt.text (1.02, est_y-2.2, r"$y=z-H\bar x_t$", ha='left', va='center',fontsize=18)
+        else:
+            plt.text (1.02, est_y-2.2, r"$y=z-\bar x_t$", ha='left', va='center',fontsize=18)
+    plt.text (0.9, est_y, "new estimate ($x_t$)", ha='right', va='center',fontsize=18)
+    plt.text (0.8, est_y-0.5, "(posterior)", ha='right', va='center',fontsize=18)
+    if show_eq:
+        plt.text (0.75, est_y-1.2, r"$\bar{x}_t + Ky$", ha='right', va='center',fontsize=18)
+    plt.xlabel('time')
+    ax.yaxis.set_label_position("right")
+    plt.ylabel('state')
+    plt.xlim(-0.5, 1.5)
+    plt.savefig('../figs/residual_chart.png', pad_inches=0.1)
+
 
 
 def show_legend():
@@ -545,6 +588,7 @@ if __name__ == "__main__":
     plot_estimate_chart_2()
     plot_estimate_chart_3()
     create_predict_update_chart()
+    show_residual_chart()
     plt.close('all')
 
     '''p = [0.2245871, 0.06288015, 0.06109133, 0.0581008, 0.09334062, 0.2245871,
--- code/mkf_internal.py
+++ code/mkf_internal.py
@@ -79,46 +79,6 @@ def plot_track_ellipses(N, zs, ps, cov, title):
     plt.show()
 
 
-def show_residual_chart(show_eq=True, show_H=False):
-    est_y = ((164.2-158)*.8 + 158)
-
-    ax = plt.axes(xticks=[], yticks=[], frameon=False)
-    ax.annotate('', xy=[1,159], xytext=[0,158],
-                arrowprops=dict(arrowstyle='->',
-                                ec='r', lw=3, shrinkA=6, shrinkB=5))
-
-    ax.annotate('', xy=[1,159], xytext=[1,164.2],
-                arrowprops=dict(arrowstyle='-',
-                                ec='k', lw=3, shrinkA=8, shrinkB=8))
-
-    ax.annotate('', xy=(1., est_y), xytext=(0.9, est_y),
-                arrowprops=dict(arrowstyle='->', ec='#004080',
-                                lw=2,
-                                shrinkA=3, shrinkB=4))
-
-
-    plt.scatter ([0,1], [158.0,est_y], c='k',s=128)
-    plt.scatter ([1], [164.2], c='b',s=128)
-    plt.scatter ([1], [159], c='r', s=128)
-    plt.text (1.05, 158.8, r"prior $(\bar{x}_t)$", ha='center',va='top',fontsize=18,color='red')
-    plt.text (0.5, 159.6, "prediction", ha='center',va='top',fontsize=18,color='red')
-    plt.text (1.0, 164.4, r"measurement ($z$)",ha='center',va='bottom',fontsize=18,color='blue')
-    plt.text (0, 157.8, r"posterior ($x_{t-1}$)", ha='center', va='top',fontsize=18)
-    plt.text (1.02, est_y-1.5, "residual($y$)", ha='left', va='center',fontsize=18)
-    if show_eq:
-        if show_H:
-            plt.text (1.02, est_y-2.2, r"$y=z-H\bar x_t$", ha='left', va='center',fontsize=18)
-        else:
-            plt.text (1.02, est_y-2.2, r"$y=z-\bar x_t$", ha='left', va='center',fontsize=18)
-    plt.text (0.9, est_y, "new estimate ($x_t$)", ha='right', va='center',fontsize=18)
-    plt.text (0.8, est_y-0.5, "(posterior)", ha='right', va='center',fontsize=18)
-    if show_eq:
-        plt.text (0.75, est_y-1.2, r"$\bar{x}_t + Ky$", ha='right', va='center',fontsize=18)
-    plt.xlabel('time')
-    ax.yaxis.set_label_position("right")
-    plt.ylabel('state')
-    plt.xlim(-0.5, 1.5)
-    plt.show()
 
 
 def plot_gaussian_multiply():
