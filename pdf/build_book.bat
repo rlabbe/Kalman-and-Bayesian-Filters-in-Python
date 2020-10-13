e27f5b46565c51aefc1021c233de6413da445d22
Roger Labbe <rlabbejr@gmail.com>
2020-10-13 11:55:54 -0700

    Added comments for how to currently build book

--- pdf/build_book.bat
+++ pdf/build_book.bat
@@ -2,6 +2,13 @@ call run_notebooks
 cd ..
 ipython merge_book.py
 
-jupyter nbconvert --to latex --template book book.ipynb
-ipython to_pdf.py
-move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
+#jupyter nbconvert --to latex --template book book.ipynb
+#ipython to_pdf.py
+#move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
+
+
+rem This is not currently working on my machine. For now I have to do this:
+rem $ jupyter nbconvert --to pdf --template book book.ipynb
+rem open book.tex in texworks editor.
+rem run lualatex conversion twice. Second time creates the TOC.
+rem move /Y book.pdf ../Kalman_and_Bayesian_Filters_in_Python.pdf
